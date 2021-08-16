9c8f9e8fff2b82bf1f59028597f219b5d929ee18
nurota <hungnguyendnipr101196@gmail.com>
2021-08-16 09:35:11 +0700

    build ui user

--- frontend-user/src/components/Base/HeaderLandingLayout/HeaderLandingLayout.tsx
+++ frontend-user/src/components/Base/HeaderLandingLayout/HeaderLandingLayout.tsx
@@ -1,20 +1,39 @@
 import useStyles from './styles';
-import { useState } from 'react'
 
-const BrightStartIcon = "/images/bright-star.svg";
-const logo = '/images/logo-red-kite.svg';
-const iconClose = "/images/icons/close.svg";
+const logo = '/images/gamefi.png';
+const ethIcon = '/images/icons/eth.svg';
 
 const HeaderDefaultLayout = () => {
   const styles = useStyles();
-  const [showBanner, setShowBanner] = useState(true)
 
   return (
     <>
       <div className={styles.navBar}>
-        <div>
+        <div className="logo">
+          <a href={"#/"}><img src={logo} alt="" /></a>
+        </div>
+        <nav className={styles.headerNav}>
+          <ul className={styles.headerLinks}>
+            <li className={styles.headerLink}><a href="#">Home</a></li>
+            <li className={styles.headerLink}><a href="#">Token sales</a></li>
+            <li className={styles.headerLink}><a href="#">Game Items Sales</a></li>
+            <li className={styles.headerLink}><a href="#">Marketplace</a></li>
+            <li className={styles.headerLink}><a href="#">Stake</a></li>
+          </ul>
+          <div className={styles.headerAccount}>
+            <span className={styles.headerAccText}>My Account</span>
+            <button className={styles.headerAccBtn}>
+              <span className="logo-currency">
+                <img src={ethIcon} alt="" />
+              </span>
+              <span className="balance">1.04263 ETH</span>
+              <span className="address">0x49...e6c</span>
+            </button>
+          </div>
+        </nav>
+        {/*<div>
           <div className="pool">
-            <a href="#/dashboard"><img src={BrightStartIcon} alt=""/></a>
+            <a href="#/dashboard"><img src={BrightStartIcon} alt="" /></a>
             <a href="#/dashboard"><span>&nbsp;&nbsp;Pool</span></a>
           </div>
           <div className="logo">
@@ -23,10 +42,11 @@ const HeaderDefaultLayout = () => {
           <div className="connects">
             <a href="https://t.me/polkafoundry/" target="_blank"><i className="custom-icon-telegram"></i></a>
             <a href="https://twitter.com/polkafoundry/" target="_blank"><i className="custom-icon-twitter"></i></a>
-            {/* <a href="#"><i className="custom-icon-facebook"></i></a>
-            <a href="#"><i className="custom-icon-github"></i></a> */}
+             <a href="#"><i className="custom-icon-facebook"></i></a>
+            <a href="#"><i className="custom-icon-github"></i></a> 
           </div>
         </div>
+        */}
         {/*{showBanner && <div className={styles.banner}>*/}
         {/*  <img src="/images/icons/ring.svg" alt="red-warning icon" />*/}
         {/*  <img src={iconClose} onClick={() => setShowBanner(false)} className={styles.closeBtn}/>*/}
--- frontend-user/src/components/Base/HeaderLandingLayout/styles.ts
+++ frontend-user/src/components/Base/HeaderLandingLayout/styles.ts
@@ -3,47 +3,45 @@ import { makeStyles } from '@material-ui/core';
 const useStyles = makeStyles((theme) => {
   return {
     navBar: {
-      display: 'flex',
-      flexDirection: 'row',
+      display: 'grid',
+      gridTemplateColumns: '173px auto',
       alignItems: 'center',
-      justifyContent: 'space-between',
-      padding: '10px 120px',
+      gap: '32px',
+      padding: '26px 84px',
       background: '#000',
       width: '100%',
       color: '#FFFFFF',
       font: 'normal normal bold 16px/24px DM Sans',
       position: 'relative',
-      top: 0,
-      left: 0,
       zIndex: 1,
 
-      '& > div': {
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        width: '100%',
+      // '& > div': {
+      //   display: 'flex',
+      //   flexDirection: 'row',
+      //   alignItems: 'center',
+      //   justifyContent: 'space-between',
+      //   width: '100%',
 
-        '& .pool ': {
-          display: 'flex'
-        },
+      //   '& .pool ': {
+      //     display: 'flex'
+      //   },
 
-        '& a': {
-          color: '#FFFFFF'
-        },
+      //   '& a': {
+      //     color: '#FFFFFF'
+      //   },
 
-        '& .connects i': {
-          marginLeft: '20px',
+      //   '& .connects i': {
+      //     marginLeft: '20px',
 
-          '&::before': {
-            color: '#9F9F9F'
-          },
+      //     '&::before': {
+      //       color: '#9F9F9F'
+      //     },
 
-          '&:hover::before': {
-            color: '#D01F37'
-          }
-        }
-      },
+      //     '&:hover::before': {
+      //       color: '#D01F37'
+      //     }
+      //   }
+      // },
       [theme.breakpoints.down('sm')]: {
         position: 'static',
         padding: '10px 40px',
@@ -78,68 +76,142 @@ const useStyles = makeStyles((theme) => {
         }
       },
     },
-    banner: {
-      position: 'absolute',
-      top: '80px',
-      backgroundColor: 'rgba(50, 50, 200, 0.4)',
-      width: '100%',
+    headerNav: {
       display: 'flex',
+      justifyContent: 'space-between',
       alignItems: 'center',
-      justifyContent: 'center!important',
-      padding: '10px 0',
-      left: '0'
     },
-    loginErrorBannerText: {
-      font: 'normal normal 400 14px/24px Helvetica',
+    headerLinks: {
+      margin: 0,
+      padding: 0,
       display: 'flex',
-      alignItems: 'center',
-      marginLeft: '10px',
+      gap: '32px',
+      flexWrap: 'wrap',
     },
-    btnChangeAppNetwork: {
-      padding: '0 15px',
-      background: 'transparent',
-      fontWeight: 600,
-      color: 'white',
-      cursor: 'pointer',
-      transition: '.2s all ease-in',
-      border: '1px solid #FFFFFF',
-      boxSizing: 'border-box',
-      borderRadius: '30px',
-      height: '28px',
-      display: 'flex',
-      alignItems: 'center',
-      font: 'normal normal 700 12px/14px DM Sans',
-
-      '&:focus': {
-        outline: 'none'
-      },
-    },
-    closeBtn: {
-      position: 'absolute',
-      top: '12px',
-      right: '20px'
-    },
-    [theme.breakpoints.down('xs')]: {
-      banner: {
-        top: '0',
-        position: 'relative',
-      },
-      navBar: {
-        '& > div': {
-          alignItems: 'flex-start'
-        }
-      },
-      loginErrorBannerText: {
-        flexDirection: 'column',
-        alignItems: 'center',
-        padding: '0 40px 0 20px',
-        marginLeft: '0',
-
-        '& button': {
-          marginTop: '10px'
-        }
+    headerLink: {
+      '& a': {
+        fontFamily: 'Firs Neue',
+        fontStyle: 'normal',
+        fontWeight: 'normal',
+        fontSize: '14px',
+        lineHeight: '24px',
+        color: '#FFFFFF',
+        mixBlendMode: 'normal',
       }
-    }
+    },
+    headerAccount: {
+      display: 'flex',
+      gap: '28px',
+      alignItems: 'center',
+    },
+    headerAccText: {
+      fontFamily: 'Firs Neue',
+      fontStyle: 'normal',
+      fontWeight: 'normal',
+      fontSize: '14px',
+      lineHeight: '24px',
+      color: '#FFFFFF',
+      mixBlendMode: 'normal',
+    },
+    headerAccBtn: {
+      background: '#2E2E2E',
+      borderRadius: '4px',
+      outline: 'none',
+      border: 'none',
+      padding: '4px 4px 4px 8px',
+      display: 'flex',
+      gap: '6px',
+      alignItems: 'center',
+
+      '& .logo-currency': {
+
+      },
+      '& .balance': {
+        fontFamily: 'Firs Neue',
+        fontStyle: 'normal',
+        fontWeight: 'normal',
+        fontSize: '14px',
+        lineHeight: '24px',
+        color: '#FFFFFF',
+        mixBlendMode: 'normal',
+      },
+      '& .address': {
+        display: 'block',
+        fontFamily: 'Firs Neue',
+        fontStyle: 'normal',
+        fontWeight: 'normal',
+        fontSize: '14px',
+        lineHeight: '22px',
+        color: '#FFFFFF',
+        mixBlendMode: 'normal',
+        padding: '7px 13px',
+        background: '#000000',
+        borderRadius: '4px'
+      }
+    },
+
+    // banner: {
+    //   position: 'absolute',
+    //   top: '80px',
+    //   backgroundColor: 'rgba(50, 50, 200, 0.4)',
+    //   width: '100%',
+    //   display: 'flex',
+    //   alignItems: 'center',
+    //   justifyContent: 'center!important',
+    //   padding: '10px 0',
+    //   left: '0'
+    // },
+    // loginErrorBannerText: {
+    //   font: 'normal normal 400 14px/24px Helvetica',
+    //   display: 'flex',
+    //   alignItems: 'center',
+    //   marginLeft: '10px',
+    // },
+    // btnChangeAppNetwork: {
+    //   padding: '0 15px',
+    //   background: 'transparent',
+    //   fontWeight: 600,
+    //   color: 'white',
+    //   cursor: 'pointer',
+    //   transition: '.2s all ease-in',
+    //   border: '1px solid #FFFFFF',
+    //   boxSizing: 'border-box',
+    //   borderRadius: '30px',
+    //   height: '28px',
+    //   display: 'flex',
+    //   alignItems: 'center',
+    //   font: 'normal normal 700 12px/14px DM Sans',
+
+    //   '&:focus': {
+    //     outline: 'none'
+    //   },
+    // },
+    // closeBtn: {
+    //   position: 'absolute',
+    //   top: '12px',
+    //   right: '20px'
+    // },
+    // [theme.breakpoints.down('xs')]: {
+    //   banner: {
+    //     top: '0',
+    //     position: 'relative',
+    //   },
+    //   navBar: {
+    //     '& > div': {
+    //       alignItems: 'flex-start'
+    //     }
+    //   },
+    //   loginErrorBannerText: {
+    //     flexDirection: 'column',
+    //     alignItems: 'center',
+    //     padding: '0 40px 0 20px',
+    //     marginLeft: '0',
+
+    //     '& button': {
+    //       marginTop: '10px'
+    //     }
+    //   }
+    // }
   };
 });
 

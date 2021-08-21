521ef1f247cc191557000857f3aa458e1ffb3126
nurota <hungnguyendnipr101196@gmail.com>
2021-08-21 23:09:30 +0700

    build home page

---
+++ frontend-user/src/pages/Home/UpcomingCard/style.ts
@@ -0,0 +1,76 @@
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles((theme: any) => ({
+  cardUpcoming: {
+    gap: '10px',
+    display: 'grid',
+
+    '& .cardBodyTimeEndItem': {
+      width: '100%',
+      borderRadius: '2px',
+      background: '#000000',
+      display: 'flex',
+      gap: '8px',
+      justifyContent: 'center',
+      alignItems: 'center',
+      padding: '7px 0px',
+      transition: '.3s'
+    },
+
+    '&:hover .cardBodyTimeEndItem': {
+      background: '#2E2E2E',
+    }
+  },
+  cardImgUpcoming: {
+    maxHeight: '220px',
+    '&::before, &::after': {
+      height: '6px !important',
+      width: '80px !important',
+    },
+
+    '& h4': {
+      fontFamily: 'Firs Neue',
+      fontStyle: 'normal',
+      fontWeight: 600,
+      fontSize: '16px',
+      lineHeight: '28px',
+      textAlign: 'center',
+      color: '#72F34B',
+      mixBlendMode: 'normal',
+      position: 'absolute',
+      top: '-26px',
+      left: '50%',
+      transform: 'translate(-50%)',
+    }
+  },
+  BodyTitleUpc: {
+    '& h4': {
+      fontWeight: 600,
+      fontSize: '24px',
+    },
+  },
+
+  timeEnd: {
+    display: 'flex',
+    alignItems: 'center',
+    // padding: '3px 8px',
+    borderRadius: '4px',
+    // background: '#2E2E2E',
+    fontFamily: 'Firs Neue',
+    fontStyle: 'normal',
+    color: ' #fff',
+    fontWeight: 600,
+    fontSize: '16px',
+    lineHeight: '28px',
+    mixBlendMode: 'normal',
+    '& .sp1': {
+      display: 'block',
+      marginRight: '6px'
+    }
+  },
+  price: {
+    textTransform: 'uppercase',
+  }
+}));
+
+export default useStyles;
--- frontend-user/src/utils/index.ts
+++ frontend-user/src/utils/index.ts
@@ -189,3 +189,27 @@ export const getDiffTime = (date1: Timestamp, date2: Timestamp) => {
     days, hours, minutes, seconds,
   }
 }
+
+
+export const caclDiffTime = (time: {[k in string]: any}): {[k in string]: any} => {
+  if (time.seconds === 0) {
+      time.seconds = 59;
+      if (time.minutes === 0) {
+          time.minutes = 59;
+          if (time.hours === 0) {
+              if (time.days > 0) {
+                  time.days -= 1;
+                  time.hours = 23;
+              }
+          } else {
+              time.hours -= 1;
+          }
+      } else {
+          time.minutes -= 1;
+      }
+  } else {
+      time.seconds -= 1;
+  }
+
+  return time;
+}

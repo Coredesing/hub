b90d13d704483a87dcab94e919ca61e7e1013c18
BlueChip <bluescorp1311@github.com>
2021-08-24 18:14:10 +0700

    update pool responsive

--- frontend-user/src/pages/Home/ActiveCard/style.ts
+++ frontend-user/src/pages/Home/ActiveCard/style.ts
@@ -1,76 +1,80 @@
-import { makeStyles } from '@material-ui/core';
+import { makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles((theme: any) => ({
   cardActive: {
-    display: 'grid',
-    width: '840px',
-    gridTemplateColumns: '401px auto',
-    gap: '32px',
-    [theme.breakpoints.down('md')]: {
-      width: 'unset',
-      gridTemplateColumns: '400px',
+    display: "grid",
+    width: "840px",
+    gridTemplateColumns: "401px auto",
+    gap: "32px",
+    // [theme.breakpoints.down("md")]: {
+    //   width: "unset",
+    //   gridTemplateColumns: "400px",
+    // },
+    [theme.breakpoints.down("sm")]: {
+      width: "400px",
+      gridTemplateColumns: "auto",
     },
-    [theme.breakpoints.down('sm')]: {
-      width: 'unset',
-      gridTemplateColumns: '310px',
-    }
+    [theme.breakpoints.down("xs")]: {
+      width: "319px",
+      gridTemplateColumns: "auto",
+    },
   },
   cardActiveApproved: {
-    border: '1px solid #72F34B',
-    boxShadow: '0px 4px 40px rgba(114, 243, 75, 0.12)',
-    background: '#000000'
+    border: "1px solid #72F34B",
+    boxShadow: "0px 4px 40px rgba(114, 243, 75, 0.12)",
+    background: "#000000",
   },
   timeEnd: {
-    display: 'flex',
-    alignItems: 'center',
+    display: "flex",
+    alignItems: "center",
     // padding: '3px 8px',
-    borderRadius: '4px',
+    borderRadius: "4px",
     // background: '#2E2E2E',
-    fontFamily: 'Firs Neue',
-    fontStyle: 'normal',
-    color: ' #fff',
+    fontFamily: "Firs Neue",
+    fontStyle: "normal",
+    color: " #fff",
     fontWeight: 600,
-    fontSize: '16px',
-    lineHeight: '28px',
-    mixBlendMode: 'normal',
-    '& .sp1': {
-      display: 'block',
-      marginRight: '6px'
-    }
+    fontSize: "16px",
+    lineHeight: "28px",
+    mixBlendMode: "normal",
+    "& .sp1": {
+      display: "block",
+      marginRight: "6px",
+    },
   },
   buynow: {
-    outline: 'none',
-    border: 'none',
-    borderRadius: '2px',
+    outline: "none",
+    border: "none",
+    borderRadius: "2px",
     fontWeight: 600,
-    fontSize: '14px',
-    lineHeight: '26px',
-    textAlign: 'center',
-    minWidth: '150px',
-    cursor: 'pointer',
-    height: '44px',
-    background: 'transparent',
-    '&.approved': {
-      color: '#000',
-      background: '#72f348',
-    }, 
-    '&.not-approved': {
-      border: '1px solid #72F34B',
-      color: '#72F34B',
-    }
+    fontSize: "14px",
+    lineHeight: "26px",
+    textAlign: "center",
+    minWidth: "150px",
+    cursor: "pointer",
+    height: "44px",
+    background: "transparent",
+    "&.approved": {
+      color: "#000",
+      background: "#72f348",
+    },
+    "&.not-approved": {
+      border: "1px solid #72F34B",
+      color: "#72F34B",
+    },
   },
   buyBox: {
-    alignItems: 'center',
-    [theme.breakpoints.down('sm')]: {
-      display: 'grid',
-      gridTemplateColumns: '1fr',
-      gap: '5px',
+    alignItems: "center",
+    [theme.breakpoints.down("sm")]: {
+      display: "grid",
+      gridTemplateColumns: "1fr",
+      gap: "5px",
     },
   },
   price: {
-    color: '#72F34B',
-    textTransform: 'uppercase'
-  }
+    color: "#72F34B",
+    textTransform: "uppercase",
+  },
 }));
 
 export default useStyles;
--- frontend-user/src/pages/Home/index.tsx
+++ frontend-user/src/pages/Home/index.tsx
@@ -1,55 +1,54 @@
-import React, { useState } from 'react';
-import { Button, TextField } from '@material-ui/core';
-import { withRouter } from 'react-router-dom';
-import clsx from 'clsx';
-import useStyles, { useCardStyles } from './style';
-import { ActiveCard } from './ActiveCard';
-import { UpcomingCard } from './UpcomingCard';
-import DefaultLayout from '../../components/Layout/DefaultLayout'
-import withWidth, { isWidthDown, isWidthUp } from '@material-ui/core/withWidth';
-import { CompleteCard } from './CompleteCard';
-import { Link } from '@material-ui/core';
-import { useFetchV1 } from '../../hooks/useFetch';
+import React, { useState } from "react";
+import { Button, TextField } from "@material-ui/core";
+import { withRouter } from "react-router-dom";
+import clsx from "clsx";
+import useStyles, { useCardStyles } from "./style";
+import { ActiveCard } from "./ActiveCard";
+import { UpcomingCard } from "./UpcomingCard";
+import DefaultLayout from "../../components/Layout/DefaultLayout";
+import withWidth, { isWidthDown, isWidthUp } from "@material-ui/core/withWidth";
+import { CompleteCard } from "./CompleteCard";
+import { Link } from "@material-ui/core";
+import { useFetchV1 } from "../../hooks/useFetch";
 
 type ResponseData = {
-  data: { [k: string]: any }[],
-  lastPage: number,
-  page: number,
-  perPage: number,
-  total: number
-}
+  data: { [k: string]: any }[];
+  lastPage: number;
+  page: number;
+  perPage: number;
+  total: number;
+};
 
 const Home = (props: any) => {
   const styles = { ...useStyles(), ...useCardStyles() };
 
   const {
     data: activePools = {} as ResponseData,
-    loading: loadingActivePools
-  } = useFetchV1('/pools/active-pools?limit=10&page=1');
+    loading: loadingActivePools,
+  } = useFetchV1("/pools/active-pools?limit=10&page=1");
   const {
     data: upcomingPools = {} as ResponseData,
-    loading: loadingUpcomingPools
-  } = useFetchV1('/pools/upcoming-pools?limit=10&page=1');
+    loading: loadingUpcomingPools,
+  } = useFetchV1("/pools/upcoming-pools?limit=10&page=1");
   const {
     data: compeltePools = {} as ResponseData,
-    loading: loadingcompletePools
-  } = useFetchV1('/pools/complete-sale-pools?limit=10&page=1');
+    loading: loadingcompletePools,
+  } = useFetchV1("/pools/complete-sale-pools?limit=10&page=1");
 
   return (
     <DefaultLayout>
       <section className={clsx(styles.pools, styles.section)}>
         <div className="rectangle">
-          <img src="/images/landing/rectangle-1.png" alt="" />
+          <img src="/images/landing/rectangle.png" alt="" />
         </div>
 
         <div className={styles.poolItem}>
-          <h3>Acitive Pools</h3>
+          <h3>Active Pools</h3>
 
           <div className={clsx(styles.cards, styles.cardsActive)}>
-            {
-              (activePools?.data || []).map((card, id) => <ActiveCard key={id} card={card} />)
-            }
-
+            {(activePools?.data || []).map((card, id) => (
+              <ActiveCard key={id} card={card} />
+            ))}
           </div>
         </div>
 
@@ -57,26 +56,29 @@ const Home = (props: any) => {
           <h3>Upcoming</h3>
 
           <div className={clsx(styles.cards, styles.cardsUpcoming)}>
-            {
-              (upcomingPools?.data?.length ? upcomingPools?.data : activePools?.data || []).map((card, id: number) => <UpcomingCard key={id} card={card} />)
-            }
+            {(upcomingPools?.data?.length
+              ? upcomingPools?.data
+              : activePools?.data || []
+            ).map((card, id: number) => (
+              <UpcomingCard key={id} card={card} />
+            ))}
           </div>
         </div>
       </section>
       <section className={clsx(styles.completePools, styles.section)}>
         <div className="rectangle">
-          <img src="/images/landing/rectangle.png" alt="" />
+          <img src="/images/landing/rectangle-black.png" alt="" />
         </div>
         <div className={styles.poolItem}>
           <h3>Complete Sale</h3>
 
           <div className={clsx(styles.cards, styles.completeCards)}>
-            {
-              (compeltePools?.data || []).map((card, id) => <CompleteCard key={id} card={card} />)
-            }
+            {(compeltePools?.data || []).map((card, id) => (
+              <CompleteCard key={id} card={card} />
+            ))}
           </div>
           <div className={styles.cardsActions}>
-            <Link href={'transactionLink'} className={styles.btnView}>
+            <Link href={"transactionLink"} className={styles.btnView}>
               View all pools
             </Link>
           </div>
@@ -86,19 +88,20 @@ const Home = (props: any) => {
         <div className="rectangle">
           <img src="/images/subcriber.svg" alt="" />
         </div>
-        <h3>
-          Get the Latest in Your Inbox
-        </h3>
+        <h3>Get the Latest in Your Inbox</h3>
         <div className={styles.contactForm}>
           <form action="">
-            <TextField className={styles.inputForm} label="Email" variant="outlined" placeholder="Enter your Email" />
-            <Button className={styles.btnForm}>
-              Subscribe
-            </Button>
+            <TextField
+              className={styles.inputForm}
+              label="Email"
+              variant="outlined"
+              placeholder="Enter your Email"
+            />
+            <Button className={styles.btnForm}>Subscribe</Button>
           </form>
 
           <div className={styles.alertMsg}>
-            <img src={'/images/warning-red.svg'} alt="" />
+            <img src={"/images/warning-red.svg"} alt="" />
             <span>Something wrong was happened. Please try again later!</span>
           </div>
         </div>

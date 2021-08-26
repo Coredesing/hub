21a7576204129d5734c8cdd2c9a835daee35cbbc
nurota <hungnguyendnipr101196@gmail.com>
2021-08-26 17:08:51 +0700

    fix pagination ticket

--- frontend-user/src/pages/Ticket/About.tsx
+++ frontend-user/src/pages/Ticket/About.tsx
@@ -343,7 +343,7 @@ const AboutTicket = ({ info = {} }: any) => {
             </TableBody>
           </Table>
         </TableContainer>
-        <Pagination count={Math.ceil(+winner.total || 0 / limitPage)} shape="rounded"
+        <Pagination count={Math.ceil((+winner.total || 0) / limitPage)} shape="rounded"
           onChange={onChangePage}
           className={classes.paginationNav}
           classes={{

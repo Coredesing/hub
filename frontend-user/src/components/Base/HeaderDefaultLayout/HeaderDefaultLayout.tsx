8c4fb3958fb906132a311d44ef7c4e8069909d81
nurota <hungnguyendnipr101196@gmail.com>
2021-09-10 22:08:57 +0700

    up

--- frontend-user/src/components/Base/HeaderDefaultLayout/HeaderDefaultLayout.tsx
+++ frontend-user/src/components/Base/HeaderDefaultLayout/HeaderDefaultLayout.tsx
@@ -279,6 +279,7 @@ const HeaderDefaultLayout: React.FC<any> = (props: any) => {
               <ul className={styles.headerLinks}>
                 <li className={styles.headerLink} onClick={hideNavMenu}><Link to="/">Home</Link></li>
                 <li className={styles.headerLink} onClick={hideNavMenu}><Link to="/pools/ticket">Ticket sales</Link></li>
+                <li className={styles.headerLink} onClick={hideNavMenu}><Link to="/pools/token">Token sales</Link></li>
                 {/* <li className={styles.headerLink} onClick={hideNavMenu}><Link to="/pools/items">Items Sales</Link></li> */}
                 {/* <li className={styles.headerLink}><Link href="#">Marketplace</Link></li> */}
                 {/* <li className={styles.headerLink}><Link href="#">Stake</Link></li> */}

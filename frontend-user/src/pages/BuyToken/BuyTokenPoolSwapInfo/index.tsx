f3f1bb6ef1272af994d8c875c5594a42ae3bbc63
nurota <hungnguyendnipr101196@gmail.com>
2021-09-10 19:24:27 +0700

    up

--- frontend-user/src/pages/BuyToken/BuyTokenPoolSwapInfo/index.tsx
+++ frontend-user/src/pages/BuyToken/BuyTokenPoolSwapInfo/index.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import BigNumber from 'bignumber.js';
 import useTokenSoldProgress from '../hooks/useTokenSoldProgress';
 import { numberWithCommas } from '../../../utils/formatNumber';
@@ -17,7 +17,13 @@ const BuyTokenPoolSwapInfo: FC<Props> = ({poolDetails, currencyName}) => {
     poolDetails?.networkAvailable,
     poolDetails,
   );
-
+    console.log(poolDetails)
+  const showProgress = (sold:number, totalSold: number) => {
+    const _sold = new BigNumber(sold);
+    const _totalSold = new BigNumber(totalSold);
+    const percent = _sold.multipliedBy(100).dividedBy(_totalSold)
+    return percent.lt(100) ? percent.toFixed(0) : 100;
+  }
   return (
     <section className={styles.sectionBuyTokenPoolSwapInfo}>
       <h2 className={styles.title}>Swap Info</h2>
@@ -71,7 +77,8 @@ const BuyTokenPoolSwapInfo: FC<Props> = ({poolDetails, currencyName}) => {
 
         <div className={styles.jubValue}>
           <div className={styles.leftBotSec}>
-            {numberWithCommas(new BigNumber(soldProgress).gte(100) ? '100': soldProgress)}%
+            {/* {numberWithCommas(new BigNumber(soldProgress).gte(100) ? '100': soldProgress)}% */}
+            { showProgress(+poolDetails.tokenSold || 0, +poolDetails.totalSoldCoin || 0)}%
           </div>
           <div className={styles.rightBotSec}>
             {
@@ -82,7 +89,7 @@ const BuyTokenPoolSwapInfo: FC<Props> = ({poolDetails, currencyName}) => {
         </div>
 
         <div className={styles.progress}>
-          <div className={styles.achieved} style={{ width: `${new BigNumber(soldProgress).gte(100) ? '100': soldProgress}%` }}></div>
+          <div className={styles.achieved} style={{ width: `${ showProgress(+poolDetails.tokenSold || 0, +poolDetails.totalSoldCoin || 0)}%` }}></div>
         </div>
       </div>
     </section>

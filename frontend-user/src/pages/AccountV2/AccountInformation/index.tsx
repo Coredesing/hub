3d21833696437572174f43ca77eacdcb6f7a0e07
hungng14m <hungng14@gmail.com>
2021-10-11 14:21:50 +0700

    hide kyc alert at myaccount

--- frontend-user/src/pages/AccountV2/AccountInformation/index.tsx
+++ frontend-user/src/pages/AccountV2/AccountInformation/index.tsx
@@ -2,14 +2,14 @@ import { useEffect, useState } from 'react';
 import _ from 'lodash';
 import useAuth from '../../../hooks/useAuth';
 import ModalVerifyEmail from '../ModalVerifyEmail';
-import {Button, withWidth, TextField, Hidden} from '@material-ui/core';
+import { Button, withWidth, TextField, Hidden } from '@material-ui/core';
 import { KYC_STATUS } from '../../../constants';
 import useWalletSignature from '../../../hooks/useWalletSignature';
 import axios from '../../../services/axios';
 import { useWeb3React } from '@web3-react/core';
 import { useDispatch } from 'react-redux';
 import { alertFailure } from '../../../store/actions/alert';
-import {useForm} from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 import useStyles from './style';
 import { useTabStyles } from '../style';
 
@@ -18,7 +18,7 @@ const AccountInformation = (props: any) => {
   const tabStyles = useTabStyles();
   const dispatch = useDispatch();
   const [openModalVerifyEmail, setOpenModalVerifyEmail] = useState(false);
-  const { connectedAccount} = useAuth();
+  const { connectedAccount } = useAuth();
   const [onEditProfile, setOnEditProfile] = useState(false);
   const { signature, signMessage } = useWalletSignature();
   const { account } = useWeb3React();
@@ -41,32 +41,32 @@ const AccountInformation = (props: any) => {
   } = props;
 
   useEffect(() => {
-    if(!connectedAccount) {
+    if (!connectedAccount) {
       setOnEditProfile(false)
     }
-  },[connectedAccount])
+  }, [connectedAccount])
 
   const { register, errors, handleSubmit } = useForm({
     mode: 'onChange'
   });
-  
+
   const onSetEditProfile = async () => {
-    if(!signature) {
+    if (!signature) {
       await signMessage();
     } else {
       setOnEditProfile(true);
       setUpdatedSuccess(false);
     }
   };
-  
+
   useEffect(() => {
-    if(signature && connectedAccount) {
+    if (signature && connectedAccount) {
       setOnEditProfile(true);
       setUpdatedSuccess(false);
     }
-  },[signature, connectedAccount]);
+  }, [signature, connectedAccount]);
 
-  const handleFormSubmit = async (data: any) =>  {
+  const handleFormSubmit = async (data: any) => {
     if (signature) {
       const config = {
         headers: {
@@ -106,7 +106,7 @@ const AccountInformation = (props: any) => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.headPage}>
-      <h2 className={tabStyles.tabTitle}>My profile</h2>
+        <h2 className={tabStyles.tabTitle}>My profile</h2>
         {
           connectedAccount && !onEditProfile && isKYC &&
           <Hidden smDown>
@@ -121,78 +121,88 @@ const AccountInformation = (props: any) => {
       <div className={styles.mainInfomation}>
         <div className={styles.inputGroup}>
           <span>Your Wallet Address</span>
-          <span style={{wordBreak: 'break-word'}}>{connectedAccount}</span>
+          <span style={{ wordBreak: 'break-word' }}>{connectedAccount}</span>
         </div>
-
-        <div className={styles.inputGroup}>
-          <span>KYC Status</span>
-          {connectedAccount && <div className="flex">
-            {(!kycStatus || kycStatus === KYC_STATUS.INCOMPLETE) && 
+        {(!kycStatus || kycStatus === KYC_STATUS.INCOMPLETE) &&
+          <div className={styles.inputGroup}>
+            <span>KYC</span>
+            <span style={{ wordBreak: 'break-word' }}>KYC is only required when you apply for the IDO Whitelists.</span>
+          </div>
+        }
+        {
+          connectedAccount && kycStatus === KYC_STATUS.APPROVED && <div className={styles.inputGroup}>
+            <span>KYC Status</span>
+            <div className="flex">
+              {/* {(!kycStatus || kycStatus === KYC_STATUS.INCOMPLETE) &&
               <span className="unverified">
                 Unverified
                 <img className={styles.iconStatus} src="/images/account_v3/icons/icon_unverified.svg" alt="" />
               </span>
             }
-            {(!kycStatus || kycStatus === KYC_STATUS.INCOMPLETE) && <button className="verify-email" onClick={handleKYC}>KYC Now</button>}
+            {(!kycStatus || kycStatus === KYC_STATUS.INCOMPLETE) && <button className="verify-email" onClick={handleKYC}>KYC Now</button>} */}
 
-            {kycStatus === KYC_STATUS.APPROVED && 
-              <span className="verified">
-                Verified
-                <img className={styles.iconStatus} src="/images/account_v3/icons/icon_verified.svg" alt="" />
-              </span>
-            }
-
-            {kycStatus === KYC_STATUS.RESUBMIT && <span style={{ color: 'red', overflow: 'unset' }}>Rejected</span>}
-          </div>}
-        </div>
-        <div className={styles.inputGroup}>
-          <span>Email Address</span>
-          {
-            connectedAccount && <>
-              {
-                (!kycStatus || kycStatus === KYC_STATUS.INCOMPLETE)
-                  ?
-                  <span>Your email used to complete KYC on Blockpass will be automatically reflected here. <br/>Please complete KYC first.</span>
-                  :
-                  <span className={styles.nameSocial}>{email}</span>
+              {kycStatus === KYC_STATUS.APPROVED &&
+                <span className="verified">
+                  Verified
+                  <img className={styles.iconStatus} src="/images/account_v3/icons/icon_verified.svg" alt="" />
+                </span>
               }
-            </>
-          }
-        </div>
 
+              {kycStatus === KYC_STATUS.RESUBMIT && <span style={{ color: 'red', overflow: 'unset' }}>Rejected</span>}
+            </div>
+          </div>
+        }
+
+        {connectedAccount && kycStatus === KYC_STATUS.APPROVED &&
+          <div className={styles.inputGroup}>
+            <span>Email Address</span>
+            {
+              connectedAccount && <>
+                <span className={styles.nameSocial}>{email}</span>
+                {/* {
+                  (!kycStatus || kycStatus === KYC_STATUS.INCOMPLETE)
+                    ?
+                    <span>Your email used to complete KYC on Blockpass will be automatically reflected here. <br />Please complete KYC first.</span>
+                    :
+                    <span className={styles.nameSocial}>{email}</span>
+                } */}
+              </>
+            }
+          </div>
+        }
         {
           connectedAccount && kycStatus === KYC_STATUS.APPROVED &&
           <form id="hook-form" onSubmit={handleSubmit(handleFormSubmit)}>
             <div className={styles.inputGroup}>
               <span>Twitter Account</span>
               {
-                connectedAccount && 
+                connectedAccount &&
                 <>
                   {
                     onEditProfile
-                    ?
-                    <div className={styles.groupInput}>
-                      <TextField 
-                        className={styles.inputNewValue} 
-                        defaultValue={twitter}
-                        placeholder="Enter your account name, EX: account"
-                          name="twitter" 
+                      ?
+                      <div className={styles.groupInput}>
+                        <TextField
+                          className={styles.inputNewValue}
+                          defaultValue={twitter}
+                          placeholder="Enter your account name, EX: account"
+                          name="twitter"
                           inputRef={register({
-                            required: true ,
+                            required: true,
                             maxLength: {
                               value: 60,
                               message: "max 60 characters"
                             }
-                          })} 
-                      />
-                      <span className={styles.errorInput}>
-                        {
-                          errors.twitter && errors.twitter.type !== 'required' ? errors.twitter.message: renderErrorRequired(errors, 'twitter')
-                        }
-                      </span>
-                    </div>
-                    :
-                    <span className={styles.nameSocial}>{twitter ?? '-'}</span>
+                          })}
+                        />
+                        <span className={styles.errorInput}>
+                          {
+                            errors.twitter && errors.twitter.type !== 'required' ? errors.twitter.message : renderErrorRequired(errors, 'twitter')
+                          }
+                        </span>
+                      </div>
+                      :
+                      <span className={styles.nameSocial}>{twitter ?? '-'}</span>
                   }
                 </>
               }
@@ -201,33 +211,33 @@ const AccountInformation = (props: any) => {
             <div className={styles.inputGroup}>
               <span>Telegram Account</span>
               {
-                connectedAccount && 
+                connectedAccount &&
                 <>
                   {
                     onEditProfile
-                    ?
-                    <div className={styles.groupInput}>
-                      <TextField 
-                        className={styles.inputNewValue} 
-                        defaultValue={telegram}
-                        placeholder="Enter your account name, EX: account"
-                          name="telegram" 
+                      ?
+                      <div className={styles.groupInput}>
+                        <TextField
+                          className={styles.inputNewValue}
+                          defaultValue={telegram}
+                          placeholder="Enter your account name, EX: account"
+                          name="telegram"
                           inputRef={register({
-                            required: true ,
+                            required: true,
                             maxLength: {
                               value: 60,
                               message: "max 60 characters"
                             }
-                          })} 
-                      />
-                      <span className={styles.errorInput}>
-                        {
-                          errors.telegram && errors.telegram.type !== 'required' ? errors.telegram.message: renderErrorRequired(errors, 'telegram')
-                        }
-                      </span>
-                    </div>
-                    :
-                    <span>{telegram ?? '-'}</span>
+                          })}
+                        />
+                        <span className={styles.errorInput}>
+                          {
+                            errors.telegram && errors.telegram.type !== 'required' ? errors.telegram.message : renderErrorRequired(errors, 'telegram')
+                          }
+                        </span>
+                      </div>
+                      :
+                      <span>{telegram ?? '-'}</span>
                   }
                 </>
               }
@@ -239,7 +249,7 @@ const AccountInformation = (props: any) => {
           <span></span>
           {connectedAccount && <>
             <span style={{ color: 'red', display: 'inline-block' }}>
-            {kycStatus === KYC_STATUS.RESUBMIT && 'Please send information to support@gamefi.org to resubmit KYC.'}
+              {kycStatus === KYC_STATUS.RESUBMIT && 'Please send information to support@gamefi.org to resubmit KYC.'}
             </span>
           </>}
         </div>
@@ -254,20 +264,20 @@ const AccountInformation = (props: any) => {
           </Button>
         </Hidden>
       }
-      
+
       {
         connectedAccount && onEditProfile && kycStatus === KYC_STATUS.APPROVED &&
         <div className={styles.footerPage}>
-          <Button 
+          <Button
             form="hook-form"
             type="submit"
-            variant="contained" 
+            variant="contained"
             className={styles.btnUpdateProfile} onClick={() => handleFormSubmit}>
             Update Profile
           </Button>
         </div>
       }
-      
+
       <ModalVerifyEmail
         setOpenModalVerifyEmail={setOpenModalVerifyEmail}
         email={email}

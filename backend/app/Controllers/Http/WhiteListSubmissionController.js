94ce6c9a06eb577af34589c4a3dbc4daa655bda4
DUONG THANH HAI <hai.duong@icetea.io>
2021-11-17 21:06:58 +0700

    feat: update privacy

--- backend/app/Controllers/Http/WhiteListSubmissionController.js
+++ backend/app/Controllers/Http/WhiteListSubmissionController.js
@@ -58,8 +58,8 @@ class WhiteListSubmissionController {
         wallet_address,
       }
       const submission = await whitelistSubmissionService.findSubmission(submissionParams)
-      submission.user_telegram = '*****'
-      submission.user_twitter = '*****'
+      submission.user_telegram = ''
+      submission.user_twitter = ''
       return HelperUtils.responseSuccess(
         submission
       );

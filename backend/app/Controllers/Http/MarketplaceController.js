0918c9681a4fc8f4e403162f9336432ea996317b
DUONG THANH HAI <hai.duong@icetea.io>
2021-11-21 20:38:32 +0700

    feat: update solana

--- backend/app/Controllers/Http/MarketplaceController.js
+++ backend/app/Controllers/Http/MarketplaceController.js
@@ -44,9 +44,13 @@ class MarketplaceController {
 
     try {
       let data = await (new MarketplaceService).getCollectionByAddress(address);
-      return HelperUtils.responseSuccess(data);
+      if (!data) {
+        return HelperUtils.responseNotFound()
+      }
+
+      return HelperUtils.responseSuccess(data)
     } catch (e) {
-      return HelperUtils.responseErrorInternal();
+      return HelperUtils.responseErrorInternal()
     }
   }
 

0eea69f42f54b7f54375e2f62a99783e54636f71
hungng14m <hungng14@gmail.com>
2021-11-23 11:52:11 +0700

    update description serie content, box type mysterybox

--- frontend-user/src/pages/Ticket/components/ModalSeriesContent.tsx
+++ frontend-user/src/pages/Ticket/components/ModalSeriesContent.tsx
@@ -50,7 +50,8 @@ const useStyles = makeStyles((theme) => ({
             '& .content-body': {
                 maxWidth: '320px',
                 [theme.breakpoints.down('xs')]: {
-                    height: '360px',
+                    minHeight: '296px',
+                    height: 'fit-content'
                 },
                 '& .header': {
                     display: 'grid',
@@ -174,49 +175,58 @@ const ModalSeriesContent = ({ open, current = {}, seriesContent, isShowRateSerie
                 <h3 className="text-white">{currentSerie.name}</h3>
             </Box>
             <Box className={styles.bodyModal}>
-                <Box className="content">
-                    <span className="prev" onClick={onPrevSerie}>
-                        <svg width="20" height="9" viewBox="0 0 20 9" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M0.11586 4.14412L4.56019 0.0931301C4.68368 -0.00716257 4.81867 -0.0264591 4.9653 0.0352813C5.11176 0.0971028 5.1853 0.208908 5.1853 0.371023V2.96363H19.6296C19.7377 2.96363 19.8265 2.99829 19.8959 3.06774C19.9652 3.13714 20 3.22592 20 3.33391V5.55608C20 5.66404 19.9653 5.75282 19.8959 5.82214C19.8264 5.89154 19.7376 5.9262 19.6297 5.9262H5.18522V8.51877C5.18522 8.6733 5.11156 8.78511 4.96518 8.85435C4.81843 8.91633 4.68343 8.8929 4.55995 8.78511L0.115616 4.68758C0.0384712 4.61048 0 4.51805 0 4.40993C0 4.30988 0.0387135 4.22126 0.11586 4.14412Z" fill="#72F34B" />
-                        </svg>
-                    </span>
-                    <Box className="content-body">
-                        <Box className="header" style={isShowRateSerie && isShowAmountSerie ? { justifyContent: 'space-between', gridTemplateColumns: 'auto auto' } : {}}>
-                            {/* <Box className={styles.boxItem}>
+                <Box>
+                    <Box className="content">
+                        <span className="prev" onClick={onPrevSerie}>
+                            <svg width="20" height="9" viewBox="0 0 20 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M0.11586 4.14412L4.56019 0.0931301C4.68368 -0.00716257 4.81867 -0.0264591 4.9653 0.0352813C5.11176 0.0971028 5.1853 0.208908 5.1853 0.371023V2.96363H19.6296C19.7377 2.96363 19.8265 2.99829 19.8959 3.06774C19.9652 3.13714 20 3.22592 20 3.33391V5.55608C20 5.66404 19.9653 5.75282 19.8959 5.82214C19.8264 5.89154 19.7376 5.9262 19.6297 5.9262H5.18522V8.51877C5.18522 8.6733 5.11156 8.78511 4.96518 8.85435C4.81843 8.91633 4.68343 8.8929 4.55995 8.78511L0.115616 4.68758C0.0384712 4.61048 0 4.51805 0 4.40993C0 4.30988 0.0387135 4.22126 0.11586 4.14412Z" fill="#72F34B" />
+                            </svg>
+                        </span>
+                        <Box className="content-body">
+                            <Box className="header" style={isShowRateSerie && isShowAmountSerie ? { justifyContent: 'space-between', gridTemplateColumns: 'auto auto' } : {}}>
+                                {/* <Box className={styles.boxItem}>
                                 <label>Amount</label>
                                 <span>{numberWithCommas(currentSerie.amount)}</span>
                             </Box>
                             <Box className={styles.boxItem}>
                                 <span className="line"></span>
                             </Box> */}
-                            {
-                                isShowRateSerie &&
-                                <Box className={styles.boxItem}>
-                                    <label className="text-center">Rare</label>
-                                    <span className="text-center">{currentSerie.rate}%</span>
-                                </Box>
-                            }
-
-                            {
-                                isShowAmountSerie &&
-                                <Box className={styles.boxItem}>
-                                    <label className="text-center">Amount</label>
-                                    <span className="text-center">{currentSerie.amount}</span>
-                                </Box>
-                            }
-
+                                {
+                                    isShowRateSerie &&
+                                    <Box className={styles.boxItem}>
+                                        <label className="text-center">Rare</label>
+                                        <span className="text-center">{currentSerie.rate}%</span>
+                                    </Box>
+                                }
+                                {
+                                    isShowAmountSerie &&
+                                    <Box className={styles.boxItem}>
+                                        <label className="text-center">Amount</label>
+                                        <span className="text-center">{currentSerie.amount}</span>
+                                    </Box>
+                                }
+                            </Box>
+                            <Box className="wrapper-img" >
+                                <div>
+                                    <img src={currentSerie.banner} alt="" />
+                                </div>
+                            </Box>
                         </Box>
-                        <Box className="wrapper-img" >
-                            <div>
-                                <img src={currentSerie.banner} alt="" />
-                            </div>
-                        </Box>
+                        <span className="next" onClick={onNextSerie}>
+                            <svg width="20" height="9" viewBox="0 0 20 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M19.8841 4.14412L15.4398 0.0931301C15.3163 -0.00716257 15.1813 -0.0264591 15.0347 0.0352813C14.8882 0.0971028 14.8147 0.208908 14.8147 0.371023V2.96363H0.370361C0.262285 2.96363 0.173506 2.99829 0.104144 3.06774C0.0348227 3.13714 0 3.22592 0 3.33391V5.55608C0 5.66404 0.0346606 5.75282 0.104103 5.82214C0.173627 5.89154 0.262407 5.9262 0.370321 5.9262H14.8148V8.51877C14.8148 8.6733 14.8884 8.78511 15.0348 8.85435C15.1816 8.91633 15.3166 8.8929 15.44 8.78511L19.8844 4.68758C19.9615 4.61048 20 4.51805 20 4.40993C20 4.30988 19.9613 4.22126 19.8841 4.14412Z" fill="#72F34B" />
+                            </svg>
+                        </span>
                     </Box>
-                    <span className="next" onClick={onNextSerie}>
-                        <svg width="20" height="9" viewBox="0 0 20 9" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M19.8841 4.14412L15.4398 0.0931301C15.3163 -0.00716257 15.1813 -0.0264591 15.0347 0.0352813C14.8882 0.0971028 14.8147 0.208908 14.8147 0.371023V2.96363H0.370361C0.262285 2.96363 0.173506 2.99829 0.104144 3.06774C0.0348227 3.13714 0 3.22592 0 3.33391V5.55608C0 5.66404 0.0346606 5.75282 0.104103 5.82214C0.173627 5.89154 0.262407 5.9262 0.370321 5.9262H14.8148V8.51877C14.8148 8.6733 14.8884 8.78511 15.0348 8.85435C15.1816 8.91633 15.3166 8.8929 15.44 8.78511L19.8844 4.68758C19.9615 4.61048 20 4.51805 20 4.40993C20 4.30988 19.9613 4.22126 19.8841 4.14412Z" fill="#72F34B" />
-                        </svg>
-                    </span>
+                    {
+                        currentSerie.description && <Box marginTop="20px" paddingLeft="36px" paddingRight="36px">
+                            <h4 className="text-white" style={{marginBottom: '8px', fontSize: '16px'}}>Description</h4>
+                            <p className="text-white" style={{lineHeight: '20px', fontSize: '14px', maxWidth: '320px'}}>
+                                {currentSerie.description}
+                            </p>
+                        </Box>
+                    }
+
                 </Box>
             </Box>
         </CustomModal>

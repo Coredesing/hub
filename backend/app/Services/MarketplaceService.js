d6b2c88d20e4c0efa087883360f762ab6061c299
DUONG THANH HAI <hai.duong@icetea.io>
2021-12-02 19:01:48 +0700

    feat: update marketplace

--- backend/app/Services/MarketplaceService.js
+++ backend/app/Services/MarketplaceService.js
@@ -28,6 +28,10 @@ class MarketplaceService {
       builder = builder.where('type', params.type)
     }
 
+    if (params.slug) {
+      builder = builder.where('slug', params.slug)
+    }
+
     return builder
   }
 
@@ -102,14 +106,14 @@ class MarketplaceService {
     return data
   }
 
-  async getCollectionByAddress(address) {
+  async getCollectionBySlug(address) {
     if (await RedisMarketplaceUtils.existRedisMarketplaceCollectionDetail(address)) {
       let data = await RedisMarketplaceUtils.getRedisMarketplaceCollectionDetail(address)
       data = JSON.parse(data)
       return data
     }
 
-    let data = await this.buildQueryCollectionBuilder({token_address: address}).first()
+    let data = await this.buildQueryCollectionBuilder({slug: address}).first()
     if (!data) {
       return
     }

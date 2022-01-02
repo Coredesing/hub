1d43eef3df59332a1508dea3e223cd178c87459b
tino <linh.nguyen@icetea.io>
2022-01-02 15:01:11 +0700

    WIP

--- frontend-v2/components/Base/Modal/index.tsx
+++ frontend-v2/components/Base/Modal/index.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from 'react'
 import { useEffect } from 'react'
 import { useRef } from 'react'
+import styles from './style.module.scss'
 
 type Props = {
   children?: ReactNode,
@@ -13,9 +14,6 @@ const Modal = ({ children, show, toggle, className }: Props) => {
   const wrapperRef = useRef<HTMLDivElement>(null)
 
   const handleClose = () => {
-    if (close) {
-      close()
-    }
     toggle(false)
   }
 
@@ -38,23 +36,36 @@ const Modal = ({ children, show, toggle, className }: Props) => {
   }, [wrapperRef, show])
 
   return (
-    show &&
+    show && 
     <>
-      <div
-        className={`modal bg-blue-gray-800 bg-opacity-60 items-center lg:py-40 ${
-          show ? 'modal-open' : ''
-        } ${className}`}
-      >
-        <div ref={wrapperRef} className="px-10 py-8">
-          <div className="bg-mechBlue-400 px-4 py-3 w-full flex justify-between rounded-t-sm shadow-lg">
-            <div className="text-lg uppercase font-bold">{}</div>
-            <button className="ml-auto " onClick={handleClose}>
-              {/* <X className="w-4 h-4" /> */}
-            </button>
-          </div>
-          <div className="bg-mechBlue-500 rounded-b-sm px-4 pt-6 pb-4">
-            <div className="overflow-auto">{children}</div>
-            {/* {footer && <div className="pt-4">{footer}</div>} */}
+      <div className="fixed z-10 inset-0 overflow-y-auto">
+        <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+            {/* Background overlay, show/hide based on modal state.
+
+            Entering: "ease-out duration-300"
+              From: "opacity-0"
+              To: "opacity-100"
+            Leaving: "ease-in duration-200"
+              From: "opacity-100"
+              To: "opacity-0" */}
+          <div className="fixed inset-0 bg-gamefiDark-900 bg-opacity-75 transition-opacity"></div>
+
+          {/* This element is to trick the browser into centering the modal contents. */}
+          <span className="hidden sm:inline-block sm:align-middle sm:h-screen">&#8203;</span>
+
+            {/* Modal panel, show/hide based on modal state.
+
+            Entering: "ease-out duration-300"
+              From: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              To: "opacity-100 translate-y-0 sm:scale-100"
+            Leaving: "ease-in duration-200"
+              From: "opacity-100 translate-y-0 sm:scale-100"
+              To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" */}
+
+          <div ref={wrapperRef} className="dark:bg-gamefiDark-400 inline-block align-bottom rounded-sm text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+            <div className={className}>
+              {children}
+            </div>
           </div>
         </div>
       </div>
--- frontend-v2/components/Base/WalletConnector/index.tsx
+++ frontend-v2/components/Base/WalletConnector/index.tsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import { useState } from 'react'
+import Modal from '../Modal'
 
 const WalletConnector = () => {
-  // const [showModal, setShowModal] = useState(false)
+  const [showModal, setShowModal] = useState(false)
   return (
     <>
       <button
         style={{ clipPath: 'polygon(0 0, calc(100% - 10px) 0, 100% 10px, 100% 100%, 0 100%, 0 0)' }}
         className='overflow-hidden py-2 px-6 bg-gamefiGreen-500 text-gamefiDark-900 font-semibold text-sm rounded-sm hover:opacity-95 cursor-pointer'
-        // onClick={() => setShowModal(true)}
+        onClick={() => setShowModal(true)}
       >
         Connect Wallet
       </button>
-      {/* <Modal show={showModal} toggle={setShowModal}></Modal> */}
+      <Modal show={showModal} toggle={setShowModal} className='px-4 py-2'>
+        <div>Wallet Connector</div>
+      </Modal>
     </>
   )
 }

14c5b29ed9a691f9a9e7748e64354693fe6f72e3
Hung Pham <hungpv@smartosc.com>
2022-01-12 19:55:30 +0700

    Add support filter is_featured

--- backend/app/Common/RedisMysteriousBoxUtils.js
+++ backend/app/Common/RedisMysteriousBoxUtils.js
@@ -19,6 +19,8 @@ const getRedisKeyMysteriousBoxes = (filterParams) => {
     }_${
       filterParams.network_available
     }_${
+      filterParams.is_featured
+    }_${
       filterParams.start_time
     }_${
       filterParams.finish_time

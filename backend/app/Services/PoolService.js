5154898937fcf86afccffc5bf596a1e035d32745
DUONG THANH HAI <hai.duong@icetea.io>
2022-01-13 15:09:46 +0700

    fix: update cache

--- backend/app/Services/PoolService.js
+++ backend/app/Services/PoolService.js
@@ -55,11 +55,11 @@ class PoolService {
       builder = builder.where('registed_by', '=', params.registed_by)
     }
 
-    // if (params.is_display === undefined) {
-    //   builder = builder.where('is_display', '=', Const.POOL_DISPLAY.DISPLAY);
-    // } else {
-    //   builder = builder.where('is_display', '=', params.is_display);
-    // }
+    if (params.is_display === undefined) {
+      builder = builder.where('is_display', '=', Const.POOL_DISPLAY.DISPLAY);
+    } else {
+      builder = builder.where('is_display', '=', params.is_display);
+    }
 
     if (params.token_type) {
       builder = builder.where('token_type', params.token_type)
@@ -206,7 +206,7 @@ class PoolService {
     let pools = await this.buildQueryBuilder(filterParams)
       .orderBy('priority', 'DESC')
       .orderBy('start_time', 'ASC')
-      .where('is_display', 1)
+      // .where('is_display', 1)
       .paginate(page, limit)
 
     pools = JSON.parse(JSON.stringify(pools))

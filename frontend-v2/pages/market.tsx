109461fa1e4da0918eb1516fcc82b2608e766470
tino <linh.nguyen@icetea.io>
2022-01-17 22:51:02 +0700

    WIP

--- frontend-v2/pages/market.tsx
+++ frontend-v2/pages/market.tsx
@@ -1,10 +1,25 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import Layout from 'components/Layout'
+import Image from 'next/image'
 
 const Market = () => (
   <Layout title="GameFi Market">
-    <div>
-      <h1>Market</h1>
+    <div className="relative w-full min-h-full">
+      <div
+        className="absolute top-0 right-0"
+        style={{
+          background: 'radial-gradient(74.55% 74.55% at 19.72% 25.45%, #C5BD06 0%, #00FF0A 100%)',
+          width: '250px',
+          height: '559px',
+          opacity: '0.1',
+          filter: 'blur(184px)'
+        }}
+      ></div>
+      <div className="absolute bottom-0 right-0">
+        <Image src={require('assets/images/bg-item-market.png')} width="221" height="247" alt=""></Image>
+      </div>
+      <div className="md:px-4 lg:px-16 md:container mx-auto lg:block">
+        <Image src={require('assets/images/market-banner.png')} alt=""></Image>
+      </div>
     </div>
   </Layout>
 )

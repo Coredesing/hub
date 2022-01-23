3111db5b68d3e7d151002387762bc0d5f125709a
tino <linh.nguyen@icetea.io>
2022-01-23 23:47:00 +0700

    feat: add loading overlay

--- frontend-v2/pages/_app.tsx
+++ frontend-v2/pages/_app.tsx
@@ -5,8 +5,33 @@ import { MyWeb3Provider } from 'components/web3/context'
 import WalletProvider from 'components/Base/WalletConnector/provider'
 import 'assets/styles/index.scss'
 import { Toaster } from 'react-hot-toast'
+import Router, { useRouter } from 'next/router'
+
+import 'react-responsive-carousel/lib/styles/carousel.min.css'
+import 'abortcontroller-polyfill/dist/polyfill-patch-fetch'
+import { useEffect, useState } from 'react'
+import LoadingOverlay from 'components/Base/LoadingOverlay'
 
 function MyApp ({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    const handleStart = (url) => (url !== router.asPath) && setLoading(true)
+    const handleComplete = (url) => setLoading(false)
+
+    router.events.on('routeChangeStart', handleStart)
+    router.events.on('routeChangeComplete', handleComplete)
+    router.events.on('routeChangeError', handleComplete)
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart)
+      router.events.off('routeChangeComplete', handleComplete)
+      router.events.off('routeChangeError', handleComplete)
+    }
+  })
+
   return (
     <Web3ReactProvider getLibrary={getLibrary}>
       <Web3ProviderNetwork getLibrary={getLibrary}>
@@ -15,6 +40,7 @@ function MyApp ({ Component, pageProps }: AppProps) {
             <Component {...pageProps} />
           </WalletProvider>
           <Toaster />
+          <LoadingOverlay loading={loading}></LoadingOverlay>
         </MyWeb3Provider>
       </Web3ProviderNetwork>
     </Web3ReactProvider>

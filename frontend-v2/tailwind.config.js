6599e4dc4a0331de3970309bfcc1650bd303d067
tino <linh.nguyen@icetea.io>
2022-01-24 12:08:38 +0700

    chore: lint

--- frontend-v2/tailwind.config.js
+++ frontend-v2/tailwind.config.js
@@ -5,52 +5,52 @@ module.exports = {
   darkMode: 'class',
   content: [
     './pages/**/*.{js,jsx,ts,tsx,vue}',
-    './components/**/*.{js,jsx,ts,tsx,vue}',
+    './components/**/*.{js,jsx,ts,tsx,vue}'
   ],
   theme: {
     extend: {
       fontFamily: {
-        'casual': ['Poppins'],
-        'mechanic': ['Rajdhani'],
+        casual: ['Poppins'],
+        mechanic: ['Rajdhani']
       }
     },
     colors: {
-      'gamefiDark': {
+      gamefiDark: {
         DEFAULT: '#13141f',
-        '300': '#555d78',
-        '400': '#4a5169',
-        '500': '#40465a',
-        '600': '#34344A',
-        '650': '#23252B',
-        '700': '#24262F',
-        '800': '#20232d',
-        '900': '#13141f'
+        300: '#555d78',
+        400: '#4a5169',
+        500: '#40465a',
+        600: '#34344A',
+        650: '#23252B',
+        700: '#24262F',
+        800: '#20232d',
+        900: '#13141f'
       },
-      'gamefiGreen': {
+      gamefiGreen: {
         DEFAULT: '#72F34B',
-        '50': '#F9FEF7',
-        '100': '#EAFDE4',
-        '200': '#CCFBBE',
-        '300': '#AEF898',
-        '400': '#90F671',
-        '500': '#72F34B',
-        '600': '#49EF16',
-        '700': '#6CDB00',
-        '800': '#288C09',
-        '900': '#195806'
+        50: '#F9FEF7',
+        100: '#EAFDE4',
+        200: '#CCFBBE',
+        300: '#AEF898',
+        400: '#90F671',
+        500: '#72F34B',
+        600: '#49EF16',
+        700: '#6CDB00',
+        800: '#288C09',
+        900: '#195806'
       },
-      'gamefiYellow': {
+      gamefiYellow: {
         DEFAULT: '#FFB800',
-        '50': '#FFEBB8',
-        '100': '#FFE5A3',
-        '200': '#FFDA7A',
-        '300': '#FFCF52',
-        '400': '#FFC329',
-        '500': '#FFB800',
-        '600': '#C79000',
-        '700': '#8F6700',
-        '800': '#573F00',
-        '900': '#1F1600'
+        50: '#FFEBB8',
+        100: '#FFE5A3',
+        200: '#FFDA7A',
+        300: '#FFCF52',
+        400: '#FFC329',
+        500: '#FFB800',
+        600: '#C79000',
+        700: '#8F6700',
+        800: '#573F00',
+        900: '#1F1600'
       },
       ...colors
     }
@@ -58,6 +58,6 @@ module.exports = {
   plugins: [
     require('@tailwindcss/line-clamp'),
     require('@tailwindcss/forms'),
-    require("@tailwindcss/aspect-ratio")
-  ],
+    require('@tailwindcss/aspect-ratio')
+  ]
 }

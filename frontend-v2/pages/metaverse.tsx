39b814ec6c143404bba23f663643cd167080febf
tino <linh.nguyen@icetea.io>
2022-01-24 16:05:16 +0700

    chore: optimize image

--- frontend-v2/pages/metaverse.tsx
+++ frontend-v2/pages/metaverse.tsx
@@ -4,10 +4,10 @@ import Image from 'next/image'
 const Metaverse = () => (
   <Layout title="GameFi Metaverse">
     <div className="relative w-full min-h-full">
-      <div className="container absolute top-20 left-0 right-0 mx-auto z-10 opacity-50">
+      <div className="container absolute top-20 left-0 right-0 mx-auto opacity-50 flex items-center justify-center" style={{ zIndex: '1' }}>
         <Image src={require('assets/images/coming-soon.png')} alt=""></Image>
       </div>
-      <div className="container absolute top-52 left-0 right-0 mx-auto z-20 uppercase text-center font-bold" style={{ fontSize: '84px', lineHeight: '80%' }}>
+      <div className="container absolute top-48 left-0 right-0 mx-auto uppercase text-center font-bold" style={{ fontSize: '74px', lineHeight: '80%', zIndex: '2' }}>
         Metaverse <br></br>Coming Soon
       </div>
       <Image

18744d8df7abb49b5da3765c3e2936e9be05cf12
tino <linh.nguyen@icetea.io>
2022-01-25 17:07:18 +0700

    fix:typo

--- frontend-v2/pages/ino/index.tsx
+++ frontend-v2/pages/ino/index.tsx
@@ -5,7 +5,7 @@ import List, { TOKEN_TYPE } from 'components/Pages/INO/List'
 import ListAuction from 'components/Pages/INO/ListAuction'
 import ListOpening from 'components/Pages/INO/ListOpening'
 import ListUpcoming from 'components/Pages/INO/ListUpcoming'
-import { useFetch } from 'components/Pages/INO/utils'
+import { useAxiosFetch } from 'components/Pages/INO/utils'
 
 const INO = () => {
   const [now, setNow] = useState<Date>(new Date())
@@ -15,7 +15,7 @@ const INO = () => {
     return () => window.clearInterval(interval.current)
   }, [])
 
-  const { data } = useFetch(`/pools/mysterious-box?token_type=${TOKEN_TYPE}&limit=10&is_featured=1&is_display=1`)
+  const { data } = useAxiosFetch(`/pools/mysterious-box?token_type=${TOKEN_TYPE}&limit=10&is_featured=1&is_display=1`)
 
   return (
     <Layout title="GameFi INO">

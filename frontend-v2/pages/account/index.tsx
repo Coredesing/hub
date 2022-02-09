d49fedad019f5e4100cf9d2533876110b60b7ffa
hungng14 <hungng14>
2022-02-09 16:47:11 +0700

    update my account

--- frontend-v2/pages/account/index.tsx
+++ frontend-v2/pages/account/index.tsx
@@ -3,12 +3,13 @@ import LeftSideBar from '@/components/Pages/Account/LeftSideBar'
 import Layout from '@/components/Layout'
 import WrapperAccount from '@/components/Pages/Account/WrapperAccount'
 import AccountContent from '@/components/Pages/Account/AccountContent'
+import Profile from '@/components/Pages/Account/Profile'
 const Account = () => {
   return <Layout title="My Account">
     <WrapperAccount>
       <LeftSideBar />
       <AccountContent>
-
+        <Profile />
       </AccountContent>
     </WrapperAccount>
   </Layout>

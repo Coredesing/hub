6a3811ca0be4c535922a1d77576f9d8b629832c1
hiendv <hien.dao@icetea.io>
2022-02-10 15:29:23 +0700

    fix: update mobile game carousel viewmore

--- frontend-v2/components/Pages/Home/GameCarousel/index.tsx
+++ frontend-v2/components/Pages/Home/GameCarousel/index.tsx
@@ -147,10 +147,12 @@ const GameCarousel = ({ items, likes }: Props) => {
                 <p className="text-left leading-5 md:text-xs lg:text-base text-gray-300 max-h-24 overflow-y-scroll">{item.short_description}</p>
               </div>
               <div className="mt-3">
-                <button className="bg-gamefiGreen-500 text-gamefiDark-900 py-2 px-6 flex align-middle items-center rounded-xs clipped-t-r hover:opacity-90">
-                  <div className="mr-2 uppercase font-bold text-xs">View more</div>
-                  <Image src={require('@/assets/images/icons/arrow-right-dark.svg')} alt="right" />
-                </button>
+                <Link href={`/aggregator/${item?.slug}`} passHref>
+                  <button className="bg-gamefiGreen-500 text-gamefiDark-900 py-2 px-6 flex align-middle items-center rounded-xs clipped-t-r hover:opacity-90">
+                    <div className="mr-2 uppercase font-bold text-xs">View more</div>
+                    <Image src={require('@/assets/images/icons/arrow-right-dark.svg')} alt="right" />
+                  </button>
+                </Link>
               </div>
             </div>
           </div>

3f9c9d3975c368641f9e9dcceb2d62b26577a3aa
tino <linh.nguyen@icetea.io>
2022-02-11 18:55:53 +0700

    chore: update ui

--- frontend-v2/pages/account/profile/index.tsx
+++ frontend-v2/pages/account/profile/index.tsx
@@ -1,18 +1,27 @@
 import React from 'react'
 import LeftSideBar from 'components/Pages/Account/LeftSideBar'
 import Layout from 'components/Layout'
-import WrapperAccount from 'components/Pages/Account/WrapperAccount'
 import AccountContent from 'components/Pages/Account/AccountContent'
 import Profile from 'components/Pages/Account/Profile'
+import { useScreens } from '@/components/Pages/Home/utils'
+import TopSideBar from '@/components/Pages/Account/TopSideBar'
 
 const ProfilePage = () => {
+  const screens = useScreens()
   return <Layout title="My Account: Profile">
-    <WrapperAccount>
-      <LeftSideBar />
-      <AccountContent>
-        <Profile />
-      </AccountContent>
-    </WrapperAccount>
+    {(screens.mobile || screens.tablet)
+      ? <div className="px-1 md:px-4 lg:px-16 md:container mx-auto pb-4">
+        <TopSideBar />
+        <AccountContent>
+          <Profile />
+        </AccountContent>
+      </div>
+      : <div className="px-1 md:px-4 lg:px-16 md:container mx-auto pb-4" style={{ display: 'grid', gridTemplateColumns: '235px 1fr' }}>
+        <LeftSideBar />
+        <AccountContent>
+          <Profile />
+        </AccountContent>
+      </div>}
   </Layout>
 }
 

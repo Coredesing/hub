2d6351b7d83b135441eece5321b4e8040a401e34
tino <linh.nguyen@icetea.io>
2022-02-15 20:36:58 +0700

    chore: update ui

--- frontend-v2/components/Layout/index.tsx
+++ frontend-v2/components/Layout/index.tsx
@@ -12,26 +12,25 @@ type Props = {
 
 const Layout = ({ children, title }: Props) => {
   const theme = 'dark'
-  return (typeof window !== undefined &&
-    <div className={`flex w-full h-screen ${theme}`}>
-      <div className="dark:bg-gamefiDark-900 dark:text-white w-full h-full flex flex-col md:flex-row">
-        <Head>
-          <title>{title}</title>
-          <meta charSet="utf-8" />
-          <link rel="shortcut icon" href="/favicon.ico" />
-          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        </Head>
-        <div>
-          <Sidebar></Sidebar>
-        </div>
-        <div className="w-full h-full overflow-auto">
-          <Topbar></Topbar>
-          {typeof window !== undefined && children}
-          <Footer></Footer>
-        </div>
-        <Toolbox></Toolbox>
+  return (<div className={`flex w-full h-screen ${theme}`}>
+    <div className="dark:bg-gamefiDark-900 dark:text-white w-full h-full flex flex-col md:flex-row">
+      <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <div>
+        <Sidebar></Sidebar>
       </div>
+      <div className="w-full h-full overflow-auto">
+        <Topbar></Topbar>
+        {children}
+        <Footer></Footer>
+      </div>
+      <Toolbox></Toolbox>
     </div>
+  </div>
   )
 }
 

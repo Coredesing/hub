6fd6f7a5dce83a8d475a618ce2987347cd7615a3
tino <linh.nguyen@icetea.io>
2022-02-28 13:56:11 +0700

    WIP: approve

--- frontend-v2/components/Pages/IGO/SwapToken.tsx
+++ frontend-v2/components/Pages/IGO/SwapToken.tsx
@@ -1,7 +1,12 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useMemo, useRef, useState } from 'react'
 import Recaptcha from '@/components/Base/Recaptcha'
 import { useFetch } from '@/utils'
 import { useMyWeb3 } from '@/components/web3/context'
+import { getCurrency } from '@/components/web3/utils'
+import { ethers } from 'ethers'
+import { getContract } from '@/components/web3/contract'
+import ERC20_ABI from '@/components/web3/abis/ERC20.json'
+import toast from 'react-hot-toast'
 
 const SwapToken = ({ poolData, userTier, loading }: { poolData: any; userTier: any; loading: boolean }) => {
   const [captchaToken, setCaptchaToken] = useState('')
@@ -9,6 +14,7 @@ const SwapToken = ({ poolData, userTier, loading }: { poolData: any; userTier: a
   const [swapable, setSwapable] = useState(false)
 
   const [currentTime, setCurrentTime] = useState(new Date())
+  const { account, library } = useMyWeb3()
 
   const recaptchaRef: any = useRef()
 
@@ -36,6 +42,46 @@ const SwapToken = ({ poolData, userTier, loading }: { poolData: any; userTier: a
     return clearInterval(clock)
   }, [currentTime, loading, poolData, userTier])
 
+  const handleApprove = async () => {
+    const token = getCurrency(poolData)
+    if (token && !token.address) {
+      token.address = ethers.constants.AddressZero
+    }
+    const spender = poolData?.campaign_hash
+    const owner = account
+    try {
+      console.log(token, spender, owner)
+      if (token && spender && owner &&
+        ethers.utils.isAddress(owner) &&
+        ethers.utils.isAddress(spender) &&
+        ethers.utils.isAddress(token.address)
+      ) {
+        // TODO: Loading here
+
+        const contract = getContract(token.address, ERC20_ABI, library, account)
+        console.log(contract)
+
+        if (contract) {
+          const transaction = await contract.approve(spender, ethers.constants.MaxInt256)
+          const loading = toast.loading('Aproval is processing')
+
+          const result = await transaction.wait(1)
+          if (+result?.status === 1) {
+            toast.success('Token Approve Successful!')
+            toast.dismiss(loading)
+          } else {
+            toast.error('Token Approve Failed')
+            toast.dismiss(loading)
+          }
+        }
+      }
+    } catch (err: any) {
+      toast.error(err.message || 'Token Approve Failed')
+    }
+  }
+
+  const handleSwap = () => {}
+
   return (<div>
     <div className="mb-4">
       <input
@@ -48,7 +94,10 @@ const SwapToken = ({ poolData, userTier, loading }: { poolData: any; userTier: a
       ></input>
     </div>
     {swapable && <Recaptcha onChange={onVerifyCapcha} ref={recaptchaRef}></Recaptcha>}
-    <button className={`px-3 py-2 rounded-sm bg-gamefiGreen-700 text-black font-medium ${!swapable ? 'opacity-75 cursor-not-allowed' : 'hover:opacity-95 cursor-pointer'}`}>Swap</button>
+    <div className="flex w-full">
+      <button className={`px-3 py-2 rounded-sm bg-gamefiGreen-700 text-black font-medium ${!swapable ? 'opacity-75 cursor-not-allowed' : 'hover:opacity-95 cursor-pointer'}`} onClick={handleApprove}>Approve</button>
+      <button className={`px-3 py-2 rounded-sm bg-gamefiGreen-700 text-black font-medium ${!swapable ? 'opacity-75 cursor-not-allowed' : 'hover:opacity-95 cursor-pointer'}`} onClick={handleSwap}>Swap</button>
+    </div>
   </div>)
 }
 

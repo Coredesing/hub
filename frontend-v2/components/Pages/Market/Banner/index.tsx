91e79341816b4596e6010c1151e811d3498c2d10
hungng14 <hungng14>
2022-03-22 15:53:42 +0700

    up

--- frontend-v2/components/Pages/Market/Banner/index.tsx
+++ frontend-v2/components/Pages/Market/Banner/index.tsx
@@ -6,7 +6,7 @@ const Banner = () => {
   const { account } = useMyWeb3()
   return (
     <div className='w-full relative'>
-      <img src={require('@/assets/images/marketplace-banner.png').default.src} width="100%" height="574" alt="" />
+      <img src={require('@/assets/images/marketplace-banner.png').default.src} className={`${styles.imgBanner} object-contain`} width="100%" height="574" alt="" />
       {
         account && <Link href={'/account/collections/assets'} passHref>
           <a className={`${styles.viewAssets} cursor-pointer z-10 absolute left-1/2 bottom-0 py-3 text-center text-13px bg-gamefiGreen-700 font-bold uppercase rounded-sm hover:bg-gamefiGreen-700/90`}>

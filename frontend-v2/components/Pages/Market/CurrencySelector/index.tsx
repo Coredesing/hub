c88fe324c359624d959cef1267bbfb4f013b4604
hungng14 <hungng14>
2022-03-23 15:32:09 +0700

    fix show icon currency marketplace

--- frontend-v2/components/Pages/Market/CurrencySelector/index.tsx
+++ frontend-v2/components/Pages/Market/CurrencySelector/index.tsx
@@ -1,5 +1,5 @@
 import { Address, ObjectType } from '@/utils/types'
-import Image from 'next/image'
+// import Image from 'next/image'
 import React, { useCallback } from 'react'
 import { currencies } from '../constant'
 
@@ -22,7 +22,7 @@ const CurrencySelector = ({ onChange, selected, ...props }: Props) => {
       <div className="flex gap-x-1.5 bg-gamefiDark-700 rounded p-1.5" style={props.style}>
         {currencies.map(currenncy => {
           return <div key={currenncy.address} className={'flex items-center rounded flex-none cursor-pointer py-1 px-2'} onClick={() => onSelectCurrency(currenncy)} style={{ backgroundColor: isActive(currenncy) ? (currenncy.colorAlt || currenncy.color) : 'transparent' }}>
-            <div className={`flex-none w-4 h-4 relative contrast-200 brightness-200 grayscale ${isActive(currenncy) ? 'opacity-100' : 'opacity-50'} hover:opacity-100`}><Image src={currenncy.icon} alt={currenncy.name} layout="fill" /></div>
+            <div className={`flex-none w-4 h-4 relative contrast-200 brightness-200 grayscale ${isActive(currenncy) ? 'opacity-100' : 'opacity-50'} hover:opacity-100`}><img src={currenncy.icon} alt={currenncy.name} /></div>
             {isActive(currenncy) && <span className={'ml-2 text-xs'}>{currenncy.symbol}</span>}
           </div>
         })}

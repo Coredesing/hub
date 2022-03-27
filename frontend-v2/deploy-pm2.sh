15170de68d9f9adb70d6ba9613cf80431de6afa1
hiendv <hien.dv.neo@gmail.com>
2022-03-27 13:34:59 +0700

    chore: update deployment

---
+++ frontend-v2/deploy-pm2.sh
@@ -0,0 +1,10 @@
+git pull
+yarn install || exit
+BUILD_DIR=.temp yarn build || exit
+if [ ! -d ".temp" ]; then
+  echo '\033[31m temp Directory not exists!\033[0m'  
+  exit 1;
+fi
+rm -rf .next
+mv .temp .next
+pm2 reload nextjs --update-env

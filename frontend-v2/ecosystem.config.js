eca6c2fcece3c53863f2604e7159c67adf99674d
hiendv <hien.dv.neo@gmail.com>
2022-03-27 17:51:09 +0700

    fix: update deployment

--- frontend-v2/ecosystem.config.js
+++ frontend-v2/ecosystem.config.js
@@ -2,6 +2,12 @@ module.exports = {
   apps: [{
     name: 'nextjs',
     script: 'npm',
-    args: 'start'
+    args: 'start',
+    env: {
+      PORT: 3000
+    },
+    env_production: {
+      PORT: 1234
+    }
   }]
 }

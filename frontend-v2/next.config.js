280a2d0b59223bc8dc28e4568cb21dbf963b02cf
hiendv <hien.dao@icetea.io>
2022-03-30 14:52:50 +0700

    fix: update cmc assets

--- frontend-v2/next.config.js
+++ frontend-v2/next.config.js
@@ -12,7 +12,7 @@ module.exports = {
     includePaths: [path.join(__dirname, 'styles'), path.join(__dirname, 'node_modules')]
   },
   images: {
-    domains: ['i.imgur.com', 'gamefi-public.s3.amazonaws.com', 'imgur.com', 'images.ctfassets.net', 's2.coinmarketcap.com', 'gamefi.ghost.io']
+    domains: ['i.imgur.com', 'gamefi-public.s3.amazonaws.com', 'imgur.com', 'images.ctfassets.net', 's2.coinmarketcap.com', 's3.coinmarketcap.com', 'gamefi.ghost.io']
   },
   async redirects () {
     return [

403a1345886a833e77721677b26e1ecc8ad270a9
hiendv <hien.dao@icetea.io>
2022-04-04 16:39:03 +0700

    chore: cleanup

--- frontend-v2/components/Pages/INO/MarketplaceDetail.tsx
+++ frontend-v2/components/Pages/INO/MarketplaceDetail.tsx
@@ -558,7 +558,7 @@ const MarketplaceDetail = ({ tokenInfo, projectInfo }: Props) => {
           <div className='flex gap-2 items-center'>
             {isFirstEdition && <img src={projectInfo.logo} alt="" className='w-11 h-11 rounded-full bg-black' />}
             <div>
-              <label htmlFor="" className="block font-bold text-white/50 text-13px uppercase">Owner <span className={`uppercase ${isFirstEdition ? 'text-gamefiGreen-700' : 'text-orange-500'}`}>{isFirstEdition ? '(First Edition)' : countdown.isSale && '(ReSale)'}</span></label>
+              <label htmlFor="" className="block font-bold text-white/50 text-13px uppercase">Owner <span className={`uppercase ${isFirstEdition ? 'text-gamefiGreen-700' : 'text-orange-500'}`}>{isFirstEdition ? '(First Edition)' : countdown.isSale && '(Resale)'}</span></label>
               <span className="block text-base font-casual">{OwnerNFT && shortenAddress(OwnerNFT, '.', 6)}</span>
             </div>
           </div>

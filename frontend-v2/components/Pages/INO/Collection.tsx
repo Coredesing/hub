8ede209b3ce0f5c64c697252475c7c941fd1bdb0
DUONG THANH HAI <hai.duong@icetea.io>
2022-04-24 13:44:23 +0700

    fix: claim 1 nft with maximum 30 nfts

--- frontend-v2/components/Pages/INO/Collection.tsx
+++ frontend-v2/components/Pages/INO/Collection.tsx
@@ -85,7 +85,7 @@ const Collection = ({ poolInfo, collections, loading, onClaimAllNFT, onClaimNFT,
                 </div>
                 <div className='flex gap-2 flex-wrap'>
                   {
-                    (!POOL_IDS_IS_CLAIMED_ONE_BY_ONE.includes(poolInfo.id) || (ownedBox && ownedBox < 200)) &&
+                    (!POOL_IDS_IS_CLAIMED_ONE_BY_ONE.includes(poolInfo.id) || (ownedBox && ownedBox <= 30)) &&
                     <button
                       className={clsx(
                         styles.btnClaimAll,

833062d94bc985e8919caf4730db48fa874cbc63
hiendv <hien.dao@icetea.io>
2022-06-22 17:40:17 +0700

    feat: add GTM

--- frontend-v2/pages/_app.tsx
+++ frontend-v2/pages/_app.tsx
@@ -18,6 +18,7 @@ import 'abortcontroller-polyfill/dist/polyfill-patch-fetch'
 const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID
 const AW_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GOOGLE_ADS_ID
 const FB_PIXEL_ID = process.env.NEXT_PUBLIC_FB_PIXEL_ID
+const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID
 
 function MyApp ({ Component, pageProps }: AppProps) {
   // migration signature from localStorage to sessionStorage
@@ -84,7 +85,20 @@ function MyApp ({ Component, pageProps }: AppProps) {
             `}
       </Script>
       <noscript dangerouslySetInnerHTML={{
-        __html: `<img alt="fb-pixel" height="1" width="1" style={{ display: 'none' }} src="https://www.facebook.com/tr?id=803968947595373&ev=PageView&noscript=1"
+        __html: `<img alt="fb-pixel" height="1" width="1" style={{ display: 'none' }} src="https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1"
+        />`
+      }}></noscript>
+      <Script id="gtag" strategy="afterInteractive">
+        {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM_ID}');
+            `}
+      </Script>
+      <noscript dangerouslySetInnerHTML={{
+        __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display:none;visibility:hidden"></iframe>"
         />`
       }}></noscript>
 

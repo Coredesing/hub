a1cdbd628354ca537079079ca7bc542609588099
hiendv <hien.dao@icetea.io>
2022-06-22 21:36:44 +0700

    fix: typo

--- frontend-v2/components/Pages/Hub/HubListV2/Filter.tsx
+++ frontend-v2/components/Pages/Hub/HubListV2/Filter.tsx
@@ -11,11 +11,11 @@ const VERSION_RELEASED = [{ text: 'Official', value: 'official' }, { text: 'Test
 const VERIFY_STATUS = [{ text: 'Launched on GameFi.org', value: 'true' }]
 const SORT_VALUES = [
   { text: 'Recently Added', field: 'createdAt', order: 'desc' },
-  { text: 'Top player', field: 'project.tokenomic.totalHolders', order: 'desc' },
-  { text: 'New releases', field: 'topReleased', order: 'desc' },
-  { text: 'Most viewed', field: 'totalViews', order: 'desc' },
-  { text: 'Top trending', field: 'totalFavorites', order: 'desc' },
-  { text: 'Top rating', field: 'rate', order: 'desc' },
+  { text: 'Most Popular', field: 'project.tokenomic.totalHolders', order: 'desc' },
+  { text: 'New Releases', field: 'topReleased', order: 'desc' },
+  { text: 'Most Viewed', field: 'totalViews', order: 'desc' },
+  { text: 'Top Trending', field: 'totalFavorites', order: 'desc' },
+  { text: 'Top Rating', field: 'rate', order: 'desc' },
   { text: 'Token ROI', field: 'roi', order: 'desc' }
 ]
 

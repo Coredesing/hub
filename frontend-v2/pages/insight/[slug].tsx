1f8b2af8465c80041a17109f699dbf9256de5548
hiendv <hien.dao@icetea.io>
2022-06-22 11:11:36 +0700

    fix: update slug

--- frontend-v2/pages/insight/[slug].tsx
+++ frontend-v2/pages/insight/[slug].tsx
@@ -22,7 +22,7 @@ const Article = ({ post }) => {
     <div className="px-4 lg:px-16 mx-auto lg:block max-w-4xl my-8 mb-16">
       <h1 className="text-[28px] !leading-shi lg:text-4xl font-bold uppercase">{post.title}</h1>
 
-      { post.primary_author && <div className="flex gap-2 items-center mt-2 xl:mt-6">
+      {post.primary_author && <div className="flex gap-2 items-center mt-2 xl:mt-6">
         <div className="relative w-11 h-11">
           <img src={post.primary_author?.profile_image || avatar} className="w-full h-full object-cover rounded-full" alt={post.primary_author?.name}></img>
         </div>
@@ -34,7 +34,7 @@ const Article = ({ post }) => {
             {post.reading_time} min read
           </p>
         </div>
-      </div> }
+      </div>}
 
       <div className="relative mt-6">
         <img src={post.feature_image} alt={post.title} className="w-full aspect-[16/9]"></img>
@@ -43,14 +43,14 @@ const Article = ({ post }) => {
       <div className="h-px bg-gradient-to-r from-gray-300/30 mt-8"></div>
       <p className="font-bold font-casual text-base mt-2">Tags</p>
       <div className="mt-4 inline-flex gap-2 flex-wrap font-casual text-sm">
-        { post.tags && post.tags.map(tag =>
+        {post.tags && post.tags.map(tag =>
           <Link href={`/insight/tag/${tag.slug}`} passHref={true} key={tag.id}>
             <a className="px-2 py-1 bg-[#242732] cursor-pointer hover:bg-gamefiDark-650 uppercase rounded-sm" key={tag.id}>{tag.name}</a>
           </Link>
         )}
       </div>
       <div className="h-px bg-gradient-to-r from-gray-300/30 my-8"></div>
-      { post.primary_author && <div className="flex gap-4 items-center mt-2 xl:mt-6">
+      {post.primary_author && <div className="flex gap-4 items-center mt-2 xl:mt-6">
         <div className="relative w-20 h-20">
           <Image src={post.primary_author?.profile_image || avatar} layout="fill" className="rounded-full" alt={post.primary_author?.name}></Image>
         </div>
@@ -61,7 +61,7 @@ const Article = ({ post }) => {
             {post.primary_author?.bio}
           </p>}
         </div>
-      </div> }
+      </div>}
     </div>
   </Layout>
 }
@@ -69,7 +69,12 @@ const Article = ({ post }) => {
 export default Article
 
 export async function getStaticProps (context) {
-  const post = await api.posts.read({ slug: context.params.slug, include: 'authors,tags' }).catch(() => {})
+  let slug = context.params.slug
+  if (slug === 'game-hub-in-gamefi-org-improve-your-experience-to-overview-the-blockchain-gaming-world') {
+    slug = 'gamefi-org-game-hub-a-steam-like-platform-bring-together-all-blockchain-games-and-gamified-projects'
+  }
+
+  const post = await api.posts.read({ slug: context.params.slug, include: 'authors,tags' }).catch(() => { })
 
   if (!post) {
     return {

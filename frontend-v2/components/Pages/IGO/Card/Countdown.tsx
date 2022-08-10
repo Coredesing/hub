73c93b0a48608e1a4442913ee9e92bbe341545e0
hiendv <hien.dao@icetea.io>
2022-08-10 20:47:30 +0700

    fix: update intervalToDuration side

--- frontend-v2/components/Pages/IGO/Card/Countdown.tsx
+++ frontend-v2/components/Pages/IGO/Card/Countdown.tsx
@@ -8,13 +8,29 @@ const Countdown = ({ to, className }: { to: string | number; className?: string 
   const [seconds, setSeconds] = useState('00')
 
   useEffect(() => {
-    if (new Date(Number(to) * 1000).getTime() < new Date().getTime()) return
+    const toDate = new Date(Number(to) * 1000)
+    if (toDate < new Date()) {
+      setDays('00')
+      setHours('00')
+      setMinutes('00')
+      setSeconds('00')
+      return
+    }
+
     const interval = setInterval(() => {
-      if (new Date(Number(to) * 1000).getTime() < new Date().getTime()) return
+      if (toDate < new Date()) {
+        setDays('00')
+        setHours('00')
+        setMinutes('00')
+        setSeconds('00')
+        return
+      }
+
       const duration = intervalToDuration({
-        start: new Date(Number(to) * 1000),
-        end: new Date()
+        start: new Date(),
+        end: toDate
       })
+
       setDays(duration.days < 10 ? `0${duration.days}` : `${duration.days}`)
       setHours(duration.hours < 10 ? `0${duration.hours}` : `${duration.hours}`)
       setMinutes(duration.minutes < 10 ? `0${duration.minutes}` : `${duration.minutes}`)

69dd2a04ba1f7f756942f8fdc497246c8434379b
DUONG THANH HAI <hai.duong@icetea.io>
2022-08-22 21:22:28 +0700

    fix: correct title

--- frontend-v2/pages/terms-of-use.tsx
+++ frontend-v2/pages/terms-of-use.tsx
@@ -1,7 +1,7 @@
 import Layout from '@/components/Layout'
 
 const Disclaimer = () => (
-  <Layout title="GameFi.org - Privacy Policy">
+  <Layout title="GameFi.org - Terms of use">
     <div className="px-4 lg:px-16 mx-auto lg:block max-w-4xl my-8 md:my-12 lg:my-16 xl:my-24">
       <div className="uppercase font-bold text-4xl">TERMS OF USE</div>
       <p className="font-casual text-sm leading-6 opacity-80 mb-8">Version V1.0</p>
